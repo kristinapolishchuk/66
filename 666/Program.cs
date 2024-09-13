eb0603f01e74fa1887d227c50581db02577587ac
kristina <kpolishchuk577@gmail.com>
2024-09-13 12:49:45 +0300

    chasnges

--- 666/Program.cs
+++ 666/Program.cs
@@ -6,4 +6,15 @@ Console.Write("HAPPY");
 Console.ForegroundColor = ConsoleColor.Red;
 Console.Write("BIRTHDAY");
 
-Console.WriteLine("HAPPY BIRTHDAY!");
+Console.WriteLine("HAPPY BIRTHDAY");
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+// See https://aka.ms/new-console-template for more information
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+
+Console.Write("Ще не вмерла Україна, і слава, і воля!");
+Console.ForegroundColor = ConsoleColor.Red;
+
+Console.Write("Ще не вмерла Україна, і слава, і воля!");
+Console.WriteLine("Ще не вмерла Україна, і слава, і воля!");
+
